5cfbdb15e1a1129f7cc4a24ec6adf68e2e4f5755
Daniel Williamson <sporeot@outlook.com>
2022-11-24 03:57:12 +0000

    Add .gitignore, and Azure Provider to boilerplate

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.32.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
